fa95c337b3830c62cbeb6ebcd2b9114c3c508889
Jason Bao <baojason@gmail.com>
2023-11-11 09:06:42 -0600

    Update IntAttribute along with its unittests

--- template/VmfAttribute/VmfAttribute.hh
+++ template/VmfAttribute/VmfAttribute.hh
@@ -36,35 +36,57 @@ class PlainGroup : public VmfAttribute {
     virtual ~PlainGroup() = default;
 };
 
+// Calculate minium valid value based on bit width
+template<typename T, uint8_t N>
+constexpr T minVal() {
+    return (N < sizeof(T)*8) ? -(T(1)<<(N-1)) : T(1) << (N-1);
+};
+// Calculate maximum valid value based on bit width
+template<typename T, uint8_t N>
+constexpr T maxVal() {
+    return (N < sizeof(T)*8) ? ((T(1)<<(N-1)) - 1) : T(~(T(1) << (N-1)));
+};
+
+/**
+ * @Tparam T        Data type used to hold attributes
+ * @Tparam numbits  Bit width, must be within the maximum width of T
+ * @Tparam minValue Range value
+ * @Tparam maxValue Range value
+ * @Tparam N        Multiplicity
+ */
 template<typename T, uint8_t numbits, T minValue, T maxValue, uint16_t N = 1>
 class IntAttribute : public VmfPlainAttribute {
     static_assert(numbits <= sizeof(T)*8, "numbits it too big!!");
-    constexpr static T Tmin = (numbits < sizeof(T)*8) ? -(T(1)<<(numbits-1)) : T(1) << (numbits-1);
-    constexpr static T Tmax = (numbits < sizeof(T)*8) ? ((T(1)<<(numbits-1)) - 1) : T(~(T(1) << (numbits-1)));
-    //constexpr static T Tmin = T(1) << (numbits-1);
-    //constexpr static T Tmax = ~(T(1) << (numbits-1));
+    constexpr static T Tmin = minVal<T, numbits>();
+    constexpr static T Tmax = maxVal<T, numbits>();
     static_assert(minValue >= Tmin, "minValue it too small!!");
     static_assert(maxValue <= Tmax, "maxValue it too big!!");
+
    public:
     IntAttribute(T value)
     : VmfPlainAttribute(numbits*N) , values_({value}) {
         if (!isValid(value)) std::cout << "Invalid value " << int(value) << "!\n";
     }
+
     IntAttribute(T values[N])
     : VmfPlainAttribute(numbits*N) , values_(values) {
         for (int i=0; i<N; i++) {
         if (!isValid(values[i])) std::cout << "Invalid value " << int(values[i]) << "!\n";
         }
     }
+
     IntAttribute(std::array<T,N> values)
     : VmfPlainAttribute(numbits*N) , values_(values) {
         for (int i=0; i<N; i++) {
         if (!isValid(values[i])) std::cout << "Invalid value " << int(values[i]) << "!\n";
         }
     }
-    IntAttribute() : VmfPlainAttribute(numbits) {
+
+    IntAttribute() : VmfPlainAttribute(numbits*N) {
     }
+
     virtual ~IntAttribute() = default;
+
     bool encode(uint8_t *toBuffer) {
         std::stringstream ss("");
         for (int i=0; i<N; i++) {
@@ -74,27 +96,33 @@ class IntAttribute : public VmfPlainAttribute {
         std::cout << ss.str() << '\n';
         return true;
     }
+
     bool decode(uint8_t *toBuffer) {
         return false;
     }
+
+    // return a single value, only for non-array.
     T getValue() {
         return values_[0];
     }
-    // get values_ to std::array
-    /*
+
+    // return values_ to std::array, benefit unit tests
     std::array<T, N> getValues() {
         return values_;
     }
-    */
+
     // get values to plain array
     void getValues(T (&values)[N]) {
         for (int i=0; i<N; i++) values[i] = values_[i];
     }
+
+    // set single value
     bool setValue(T value) {
         if (!isValid(value)) return false;
         values_[0] = value;
         return true;
     }
+
     // set values from a plain array reference
     bool setValues(T (&values)[N]) {
         for ( int i=0; i<N; i++ ) {
@@ -103,6 +131,7 @@ class IntAttribute : public VmfPlainAttribute {
         }
         return true;
     }
+
     // set values from std::array
     bool setValues(std::array<T, N> &values) {
         for ( int i=0; i<N; i++ ) {
@@ -111,9 +140,11 @@ class IntAttribute : public VmfPlainAttribute {
         values_ = values;
         return true;
     }
+
    private:
     bool isValid(T &v) {
         return (v >= minValue && v <= maxValue);
     }
+
     std::array<T, N> values_;
 };
--- template/VmfAttribute/VmfAttributeTest.cc
+++ template/VmfAttribute/VmfAttributeTest.cc
@@ -27,66 +27,74 @@ INSTANTIATE_TEST_CASE_P(
 #endif
 
 TEST(VmfAttributeTest, IntAttribute_Constructor) {
+    IntAttribute<int8_t, 1, -1, 0> myInt1(-1);
+    EXPECT_EQ(1, myInt1.getNumberOfBits());
+    EXPECT_EQ(-1, myInt1.getValue());
+
     IntAttribute<int8_t, 7, -64, 63> myInt7(-64);
+    EXPECT_EQ(7, myInt7.getNumberOfBits());
     EXPECT_EQ(-64, myInt7.getValue());
+
     IntAttribute<int8_t, 8, -128, 127> myInt8(-128);
+    EXPECT_EQ(8, myInt8.getNumberOfBits());
     EXPECT_EQ(-128, myInt8.getValue());
-    IntAttribute<int16_t, 16, -32768, 32767> myInt15(-32768);
-    EXPECT_EQ(-32768, myInt15.getValue());
+
+    IntAttribute<int16_t, 16, -32768, 32767> myInt16(-32768);
+    EXPECT_EQ(16, myInt16.getNumberOfBits());
+    EXPECT_EQ(-32768, myInt16.getValue());
+
     IntAttribute<int32_t, 32, -2147483648, 2147483647> myInt32(-2147483648);
+    EXPECT_EQ(32, myInt32.getNumberOfBits());
     EXPECT_EQ(-2147483648, myInt32.getValue());
-    IntAttribute<int64_t, 40, -549755813888, 549755813887> myInt64(-549755813888);
-    EXPECT_EQ(-549755813888, myInt64.getValue());
-    //EXPECT_EQ(4, myInt.getNumberOfBits());
-    /*IntAttribute<int8_t, 4, -3, 10, 3> myInt2({-2,3,7});
-    EXPECT_EQ(-2, myInt2.getValue());
-    EXPECT_EQ(12, myInt2.getNumberOfBits());
-    int8_t data2[3] = {-2,3,7};
-    int8_t readValues[3];
-    myInt2.getValues(readValues);
-    for (int i=0; i<3; i++)
-        EXPECT_EQ(data2[i], readValues[i]);
-        */
-    //IntAttribute<int32_t, 20, -524289, 524288> myInt3(-524289);
-    //EXPECT_EQ(-524289, myInt3.getValue());
-}
-#if 0
-TEST(VmfAttributeTest, IntAttribute_setget) {
-    IntAttribute<int8_t, 4, -3, 10> myInt;
-    EXPECT_FALSE(myInt.setValue(-4));
-    EXPECT_TRUE(myInt.setValue(7));
-    EXPECT_EQ(7, myInt.getValue());
-    EXPECT_EQ(4, myInt.getNumberOfBits());
+
+    IntAttribute<int64_t, 40, -549755813888, 549755813887> myInt40(-549755813888);
+    EXPECT_EQ(40, myInt40.getNumberOfBits());
+    EXPECT_EQ(-549755813888, myInt40.getValue());
 }
 
-TEST(VmfAttributeTest, IntAttribute_setget2) {
-    IntAttribute<int8_t, 7, -3, 10> myInt;
+TEST(VmfAttributeTest, IntAttribute_ArrayConstructor) {
+    IntAttribute<int8_t, 3, -3, 3, 3> myInt3Array({-3,0,3});
+    EXPECT_EQ(9, myInt3Array.getNumberOfBits());
+    std::array<int8_t, 3> data = {-3,0,3};
+    EXPECT_EQ(data, myInt3Array.getValues());
+}
+
+
+TEST(VmfAttributeTest, IntAttribute_setget) {
+    IntAttribute<int8_t, 4, -3, 7> myInt;
+    EXPECT_EQ(4, myInt.getNumberOfBits());
+    EXPECT_FALSE(myInt.setValue(-4));
+    EXPECT_FALSE(myInt.setValue(8));
     EXPECT_TRUE(myInt.setValue(7));
     EXPECT_EQ(7, myInt.getValue());
-    IntAttribute<int8_t, 7, -3, 10, 3> myIntArray;
-    //std::array<int8_t, 3> data2 = {-1, 2, 7};
-    int8_t data2[3] = {-1, 2, 7};
-    //EXPECT_TRUE(myIntArray.setValues(data2));
-    EXPECT_TRUE(myIntArray.setValues(data2));
+}
+
+TEST(VmfAttributeTest, IntAttribute_setgetArray) {
+    IntAttribute<int8_t, 7, -64, 63, 3> myInt7Array;
+    EXPECT_EQ(21, myInt7Array.getNumberOfBits());
+    int8_t setValues[3] = {-64, 0, 63};
+    EXPECT_TRUE(myInt7Array.setValues(setValues));
     int8_t readValues[3];
-    myIntArray.getValues(readValues);
+    myInt7Array.getValues(readValues);
     for (int i=0; i<3; i++)
-        EXPECT_EQ(data2[i], readValues[i]);
+        EXPECT_EQ(setValues[i], readValues[i]);
 }
 
 TEST(VmfAttributeTest, IntAttribute_encode) {
-    int8_t data[3] = {-1, 2, 7};
-    IntAttribute<int8_t, 7, -3, 10> myInt(7);
+    IntAttribute<int8_t, 7, -64, 63> myInt7(35);
     uint8_t tobuf[128];
-    EXPECT_EQ(7, myInt.getValue());
-    EXPECT_TRUE(myInt.encode(tobuf));
-    std::cout << tobuf << '\n';
-    IntAttribute<int8_t, 7, -3, 10, 3> myInt2({-1,2,7});
-    int8_t readValues[3];
-    myInt2.getValues(readValues);
-    for (int i=0; i<3; i++)
-        EXPECT_EQ(data[i], readValues[i]);
-    EXPECT_TRUE(myInt2.encode(tobuf));
+    EXPECT_EQ(35, myInt7.getValue());
+    EXPECT_TRUE(myInt7.encode(tobuf));
     std::cout << tobuf << '\n';
 }
+
+TEST(VmfAttributeTest, IntAttribute_encodeArray) {
+    std::array<int8_t,3> varify = {-64, 0, 63};
+    IntAttribute<int8_t, 7, -64, 63, 3> myInt7Array({-64,0,63});
+    uint8_t tobuf[128];
+    EXPECT_EQ(varify, myInt7Array.getValues());
+    EXPECT_TRUE(myInt7Array.encode(tobuf));
+    std::cout << tobuf << '\n';
+}
+#if 0
 #endif
