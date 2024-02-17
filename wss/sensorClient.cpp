04c9e0c06dd4979710873c363d09c4b54baf1695
Jason Bao <baojason@gmail.com>
2024-02-17 09:40:25 -0600

    Add WSS interview c++ code

---
+++ wss/sensorClient.cpp
@@ -0,0 +1,113 @@
+#include <bits/stdc++.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <chrono>
+
+#define SERVER_PATH "sensors.server"
+#define CLIENT_PATH "sensor.client"
+using namespace std;
+using std::chrono::high_resolution_clock;
+using std::chrono::duration_cast;
+using std::chrono::duration;
+using std::chrono::milliseconds;
+
+int main(int argc, char** argv){
+
+    int client_sock, rc, len, sensorID;
+    struct sockaddr_un server_sockaddr;
+    struct sockaddr_un client_sockaddr;
+    char buf[256];
+    string cliStr;
+    if (argc>1) {
+        if (!strcmp(argv[1], "STOP")) {
+            sensorID = -1;
+        } else {
+            sensorID = atoi(argv[1]);
+        }
+    } else {
+        sensorID = 1;
+    }
+    cliStr = CLIENT_PATH + to_string(sensorID);
+
+    for(;;)
+    {
+        usleep(50000);
+        memset(&server_sockaddr, 0, sizeof(struct sockaddr_un));
+        memset(&client_sockaddr, 0, sizeof(struct sockaddr_un));
+        // Create a UNIX domain stream socket
+        client_sock = socket(AF_UNIX, SOCK_STREAM, 0);
+        if (client_sock == -1) {
+            cout << "FAIL TO CREATE SOCKET\n";
+            exit(1);
+        }
+
+        client_sockaddr.sun_family = AF_UNIX;
+        strcpy(client_sockaddr.sun_path, cliStr.c_str());
+        len = sizeof(client_sockaddr);
+
+        unlink(cliStr.c_str());
+        rc = bind(client_sock, (struct sockaddr *) &client_sockaddr, len);
+        if (rc == -1){
+            cout << "BIND ERROR !!!\n";
+            close(client_sock);
+            exit(1);
+        }
+
+        // Set up the UNIX sockaddr structure
+        server_sockaddr.sun_family = AF_UNIX;
+        strcpy(server_sockaddr.sun_path, SERVER_PATH);
+        rc = connect(client_sock, (struct sockaddr *) &server_sockaddr, len);
+        if(rc == -1){
+            cout << "CONNECT ERROR !!!\n";
+            close(client_sock);
+            exit(1);
+        }
+        // Copy the data to the buffer and send out
+        string cmd = to_string(sensorID);
+        if (sensorID == -1) {
+            cmd = "STOP1234567890";
+        }
+        strcpy(buf, cmd.c_str());
+        rc = send(client_sock, buf, strlen(buf), 0);
+        if (rc == -1) {
+            cout << "SEND ERROR !!!\n";
+            close(client_sock);
+            exit(1);
+        }
+        else {
+            if (sensorID == -1) {
+                cout << "STOP THE SERVER REQUEST IS SENT!!!\n";
+                break;
+            }
+            cout << "Reading sensor[" << cmd <<"]\n";
+        }
+
+        // measure the time
+        auto t1 = high_resolution_clock::now();
+        // Read the sensor sent from the server
+        memset(buf, 0, sizeof(buf));
+        rc = recv(client_sock, buf, sizeof(buf), 0);
+        auto t2 = high_resolution_clock::now();
+        // Getting number of milliseconds as a double
+        duration< double, std::milli > ms_double = t2 - t1;
+
+        if (rc == -1) {
+            cout << "RECV ERROR !!!\n";
+            close(client_sock);
+            exit(1);
+        }
+        else {
+            cout << "Senor[" << sensorID << "] = " << buf
+                 << " Waiting ... " << ms_double.count() << "ms\n";
+        }
+        close(client_sock);
+    }
+
+    return 0;
+}
---
+++ wss/wss.cpp
@@ -0,0 +1,321 @@
+/**
+ * Please create a C++ class implementing sensor query server, with the following characteristics:
+ *
+ * 1. The server is a process;
+ * 2. The server queries the sensors periodically, each sensor with its own frequency
+ * 3. The list of the sensors is supplied as the constructor/initializer
+ *    (on your choice) as a list of structures (or objects of some class, on
+ *    your choice), and doesn't change during the server's object life.
+ *    Each element of the array contains the sensor access information
+ *    (pointer or reference to the object of the GenericSensor class) and
+ *    query periodicity, in milliseconds. Please consider the GenericSensor
+ *    as an abstract class implementing
+ *    single method: float GenericSensor::getValue();
+ * 4. The measured data types may vary, but is always a floating point
+ *    value (e.g. temperature, pressure, velocity, etc.)
+ * 5. Specific implemetation of low-level access to the sensors is
+ *    separate and out of scope of this problem. You can safely assume that
+ *    the access to the sensors is thread-safe.
+ * 6. The data asquired from the sensors is stored internally in the
+ *    server object and provided on demand to the clients.
+ * 7. The server provides interface for the sensor data retrieval. The
+ *    interface must be designed in the way allowing the data receive by one
+ *    or more pther processes. Please use IPC of your choice for this
+ *    purpose.
+ *    The provided data is result of the latest query of the sensor.
+ * 8. Please use coding and comment conventions of your choice
+ * 9. The code is not required to be running, but must be complete enough
+ *    to be understood.
+ * 10. Please avoid usage of existing implementations of the servers, the
+ *    only allowed external template library is STL.
+ */
+#include <bits/stdc++.h>
+#include <thread>
+#include <unistd.h>
+#include <functional>
+#include <mutex>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <iomanip>
+#include <sstream>
+
+#define SOCK_PATH  "sensors.server"
+
+using namespace std;
+struct SensorData;
+static int SensorIDPool = 0;
+static int pollDelay = 1000000; // usecs
+/**
+ * The following code are to be mock the real sensor readings.
+ */
+/**
+ * @class   Sensor
+ * @brief   This is an abstract sensor
+ */
+class GenericSensor
+{
+private:
+    int m_ID;
+public:
+    GenericSensor() : m_ID(++SensorIDPool), m_Delay(1000), m_Data(nullptr){};
+    virtual ~GenericSensor(){};
+
+    mutex mx;
+    /**
+     * Return a value of read from hareware.
+     * Must be implemented according to the type of sensor.
+     */
+    virtual float getValue() = 0;
+    int m_Delay;
+    SensorData* m_Data;
+    int getID() { return m_ID; };
+};
+
+/**
+ * This struct is used to contain the reading value from a sensor
+ */
+struct SensorData{
+    float latestValue;          /// Is used to contain the latest reading value
+    GenericSensor* sensor;
+    SensorData() : latestValue(0.0), sensor(nullptr) {};
+    SensorData(GenericSensor* pSen) : latestValue(0.0), sensor(pSen) {
+    };
+    void setReturnData() {
+        sensor->m_Data = this;
+    };
+};
+
+/**
+ * @class   Sensor1
+ * @brief   mimic sensor1 behavior
+ */
+class Sensor1 : public GenericSensor
+{
+public:
+    Sensor1() {};
+    virtual ~Sensor1() {};
+    float getValue() {
+        float val = (float)(rand() % 1000)/2.0;
+        // Save the reading value back to a container
+        // mimic a call back
+        lock_guard<mutex> guard(mx);
+        m_Data->latestValue = val;
+        return val;
+    };
+};
+
+/**
+ * @class   Sensor2
+ * @brief   mimic sensor2 behavior
+ */
+class Sensor2 : public GenericSensor
+{
+public:
+    Sensor2() {};
+    virtual ~Sensor2() {};
+    float getValue() {
+        float val = (float)(rand() % 10000)/5.0;
+        lock_guard<mutex> guard(mx);
+        m_Data->latestValue = val;
+        return val;
+    };
+};
+
+Sensor1 obj1;
+Sensor2 obj2, obj3;
+vector<GenericSensor*> mySensors = {&obj1, &obj2, &obj3};
+
+/**
+ * @class   SensorQuerySrv
+ * @brief   This is a sensor query server.
+ *
+ */
+class SensorQuerySrv
+{
+private:
+    unordered_map<int, SensorData> m_Sensors;   /// sensor container
+    mutex m_Mtx;
+    int m_State;                                /// running state of the server
+                                                /// 0: stop
+                                                /// 1: running
+                                                /// 2: pause
+public:
+    /**
+     * Constructor
+     */
+    SensorQuerySrv(vector<GenericSensor*> sensors) : m_State(0) {
+        for (auto & s : sensors) {
+            m_Sensors.emplace(s->getID(), SensorData(s));
+            m_Sensors[s->getID()].setReturnData(); // hook up with the m_Data
+        }
+    };
+
+    /**
+     * Destructor
+     */
+    virtual ~SensorQuerySrv() {};
+
+    /**
+     * retrieve function
+     * This function is provided to multiple threads access.
+     */
+    float readSensor(int sen) {
+        if (m_Sensors.find(sen) == m_Sensors.end()) {
+            return -1.0; // Non exist sensor
+        } else {
+            float value;
+            // protect the reading value
+            lock_guard<mutex> guard(m_Sensors[sen].sensor->mx);
+            value = m_Sensors[sen].latestValue;
+            return value;
+        }
+    };
+
+    /**
+     * Update polling state
+     */
+    void updatePollingState(int state)
+    {
+        m_State = (state >= 0 && state <3) ? state : m_State;
+    };
+
+    /**
+     * Periodically running function
+     * This function will be called in a seperate thread for polling all sensors
+     */
+     void pollSensors() {
+        for (;;) {
+            if (m_State == 1) {
+                for (auto s : m_Sensors) {
+                    /**
+                     * Here, just call the sensor API to update the sensor reading.
+                     * This could be block reading, or asynchronous unblock polling.
+                     * I just ignore the detial implementations, please refer to
+                     * the above code.
+                     */
+                    s.second.sensor->getValue();
+                }
+            } else if (m_State == 0) {
+                // Stop the thread
+                break;
+            }
+            usleep(pollDelay);
+        }
+    };
+};
+
+int main()
+{
+    /**
+     * The server is implementd by UNIX domain socket.
+     * It will listen the connections. As soon as there is a connection,
+     * it will call the above class object with sensor ID, and return
+     * the read value to the client.
+     */
+    int server_sock, client_sock, rc;
+    socklen_t len;
+    int bytes_rec = 0;
+    struct sockaddr_un server_sockaddr;
+    struct sockaddr_un client_sockaddr;
+    char buf[256];
+    int backlog = 10, sensorID;
+    float retVal;
+    string cmd;
+    stringstream ss;
+    SensorQuerySrv server(mySensors);
+    server.updatePollingState(1);
+    thread senThread(&SensorQuerySrv::pollSensors, &server);
+
+    memset(&server_sockaddr, 0, sizeof(struct sockaddr_un));
+    memset(&client_sockaddr, 0, sizeof(struct sockaddr_un));
+    memset(buf, 0, 256);
+
+    // Create a domain stream socket
+    server_sock = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (server_sock == -1){
+        cout << "SOCKET ERROR WHEN IT IS CREATED ("<<strerror(errno)<<")\n";
+        exit(1);
+    }
+
+    // Set up the UNIX sockaddr structure
+    server_sockaddr.sun_family = AF_UNIX;
+    strcpy(server_sockaddr.sun_path, SOCK_PATH);
+    len = sizeof(server_sockaddr);
+    unlink(SOCK_PATH);
+    rc = bind(server_sock, (struct sockaddr *) &server_sockaddr, len);
+    if (rc == -1){
+        cout << "FAIL TO BIND SERVER ("<<strerror(errno)<<")!!!\n";
+        close(server_sock);
+        exit(1);
+    }
+
+    // listen to connections
+    rc = listen(server_sock, backlog);
+    if (rc == -1){
+        cout << "LISTEN ERROR ("<<strerror(errno)<<") !!!\n";
+        close(server_sock);
+        exit(1);
+    }
+    cout << "socket listening...\n";
+
+    while (1) {
+        // accepting the client request
+        client_sock = accept(server_sock, (struct sockaddr *) &client_sockaddr, &len);
+        if (client_sock == -1){
+            cout << "ACCEPT ERROR ("<<strerror(errno)<<")!!!\n";
+            close(server_sock);
+            close(client_sock);
+            exit(1);
+        }
+
+        // get client request cmd
+        memset(buf, 0, 256);
+        bytes_rec = recv(client_sock, buf, sizeof(buf), 0);
+        if (bytes_rec == -1){
+            cout << "RECV ERROR ("<<strerror(errno)<<")!!!\n";
+            close(server_sock);
+            close(client_sock);
+            exit(1);
+        }
+        else {
+            cmd = buf;
+            // parse the request
+            if (cmd == "STOP1234567890") {
+                // This is the command that is used to stop the server
+                server.updatePollingState(0);
+                break;
+            }
+            sensorID = stoi(cmd);
+            cout << "READ Senor[" <<  buf  << "]" << endl;
+        }
+        // set data back
+        retVal = server.readSensor(sensorID);
+        ss.str("");
+        ss.clear();
+        ss << retVal;
+        // cmd = to_string(retVal);
+
+        memset(buf, 0, 256);
+        strcpy(buf, ss.str().c_str());
+        rc = send(client_sock, buf, strlen(buf), 0);
+        if (rc == -1) {
+            cout << "SEND ERROR ("<<strerror(errno)<<")!!!\n";
+            close(server_sock);
+            close(client_sock);
+            exit(1);
+        }
+        else {
+            cout << "Data sent! "<<retVal<<"\n";
+        }
+        close(client_sock);
+    }
+    close(server_sock);
+    senThread.join();
+    cout << "\nThe sensor server is stopped !!!\n";
+    return 0;
+}
